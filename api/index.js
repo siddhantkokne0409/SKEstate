0dbe7e00cb1aafffe647d55d9fd09ae8fb0848fc
Siddhant <koknesiddhant3@gmail.com>
2024-10-09 12:26:01 +0530

    Database Initialized!!

--- api/index.js
+++ api/index.js
@@ -1,9 +1,21 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDb");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
 const port = 3000;
 
-app.listen(port,()=>{
-    console.log(`server is running on port ${port}`);
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
 });
